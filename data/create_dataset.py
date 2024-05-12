08e66d8047b9f821760877e22afde3137be3c577
transieu102 <trantran102206@gmail.com>
2024-05-12 21:54:13 +0700

    fix (create_dataset): bug free

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -1,7 +1,10 @@
 from data.data_tools import *
 from data.utils import pre_video
 import clip
+import torch
+import cv2
 from tqdm import tqdm
+import os
 def create_new_features(clip_version : str, h5_file_name : str, h5_new_file_name : str, video_original_dir: str) -> None:
     """
     change features
@@ -31,15 +34,19 @@ def create_new_features(clip_version : str, h5_file_name : str, h5_new_file_name
             # Check if the frame is read successfully
             if not ret:
                 print("Error: Unable to read frame.")
-                return 
-            features.append(extract_features(frame, model, preprocessing))
+                return
+            feature = extract_features(frame, model, preprocessing).reshape(1, -1)
+            # print(feature.shape)
+            features.append(feature)
+            # print(len(features))
         cap.release()
         video_embeddings, video_mask = pre_video(torch.tensor(features), max_frames=512)
         data[video_id]['video_embeddings'] = video_embeddings
         data[video_id]['video_mask'] = video_mask
 
         #TODO: add video caption features
-        prompt_embeddings = torch.zeros(features[0].size(1))
+        print(features)
+        prompt_embeddings = torch.zeros(features[0].shape[1])
         for frame_feature in features:
             prompt_embeddings += frame_feature
         prompt_embeddings /= len(features)
