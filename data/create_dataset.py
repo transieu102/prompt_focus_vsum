7a93d563db70716c7089676ca765df6c388c3028
transieu102 <trantran102206@gmail.com>
2024-05-13 14:18:18 +0700

    fix shape

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -35,10 +35,11 @@ def create_new_features(clip_version : str, h5_file_name : str, h5_new_file_name
             if not ret:
                 print("Error: Unable to read frame.")
                 return
-            feature = extract_features(frame, model, preprocessing)
-            # print(feature.shape)
+            feature = extract_features(frame, model, preprocessing).reshape(-1)
+            # print(feature.shape) 
             features.append(feature)
-            # print(len(features))
+        # print(len(features))
+
         cap.release()
         # video_embeddings, video_mask = pre_video(torch.tensor(features), max_frames=512)
         video_embeddings = torch.tensor(features)
